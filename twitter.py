4ee23266221d640e2e1dd795353a045e7e6dde5e
mayada_abdel_rahman <mayada.rahman93@gmail.com>
2018-04-01 20:44:20 +0200

    add twitter api client

---
+++ twitter.py
@@ -0,0 +1,15 @@
+"""
+Twitter API client.
+
+waiting for twitter app approval.
+"""
+
+
+def user_info(user_id):
+    """Returns dictionary of user info"""
+    return {}
+
+
+def tweets(user_id, limit=25):
+    """Returns list of tweets from user timeline, with limit of 25 by default"""
+    return []
