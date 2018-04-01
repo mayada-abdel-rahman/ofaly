2dfff564d87e976ca59f734e0c497c7778033a76
mayada_abdel_rahman <mayada.rahman93@gmail.com>
2018-04-01 21:09:06 +0200

    add user/tweet model

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(app)
 
 
+# ------------------- Models ------------------
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    provider_user_id = db.Column(db.String(60), index=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    tweets = db.relationship('Tweet', backref='user', lazy=True)
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.Text)
+    retweet = db.Column(db.Integer, default=0)
+    favorite = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+# ------------------- Views -------------------
 @app.route('/users')
 def get_users():
     return "All users"
