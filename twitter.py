fb4b09778c4e3399beb8692134ffcbb1f9271047
mayada_abdel_rahman <mayada.rahman93@gmail.com>
2018-04-02 22:03:28 +0200

    add fake data for testing

--- twitter.py
+++ twitter.py
@@ -7,9 +7,25 @@ waiting for twitter app approval.
 
 def user_info(user_id):
     """Returns dictionary of user info"""
-    return {}
+
+    # fake data for testing.
+    return {
+        'name': 'UserName',
+        'email': 'random@email.com'
+    }
 
 
 def tweets(user_id, limit=25):
     """Returns list of tweets from user timeline, with limit of 25 by default"""
-    return []
+
+    # fake data for testing.
+    tweets_lst = [
+        {
+            'content': 'content{}'.format(i),
+            'retweet': i + 1,
+            'favorite': i + 1,
+        }
+        for i in range(100)
+    ]
+
+    return tweets_lst[:limit]
